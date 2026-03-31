b8930e2751cf67752086c1e1a8e363a8a87779e3
kohee <kiaff450@gmail.com>
2026-03-31 10:39:46 +0000

    Now i have added this and form this I will start learning rust deeply anyhoe

--- main.rs
+++ main.rs
@@ -2,4 +2,9 @@ fn main() {
     println!("Hello, world!");
 }
 
-bro  learning rust
+
+
+
+
+//i am learning rust  form a cli os and I hope that i wil learn rust more deeper then previous time
+//at all anyhow ....i will finish rust before 2 months anyhow __ I am ready to burn in the fire
