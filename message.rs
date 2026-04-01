7c8461128156c600bae0cf50df0dd120fc0396b3
kohee <kiaff450@gmail.com>
2026-04-01 17:21:29 +0000

    Todays message at mid night

---
+++ message.rs
@@ -0,0 +1,26 @@
+
+
+
+date : 1/april/2026 
+Its mid night and I am front of the screen
+I am alone in my room right now 
+I am felling lonely 
+I am felling tired 
+Its a completely cli os and 
+I dont have any kind of ai right now 
+I Have no graphical objects 
+I have only texts here 
+I am learning rust and I in this environment i am felling very bad
+Thanks to my self that i am now fasting dopamine right now 
+and i will learn rust as soon as possible but as deep as much as i can 
+I am becomming self dependent insted of relaying on something else 
+And that is todays biggest achievement 
+I am thanking my self to work with a cli os ...I am really happy now 
+There is no distruction , no fucking shit here 
+Only something lonely but deep 
+My room is dark now ...and I am felling the rage that is comming from the 
+universal storage ...
+Its up to me 
+I never found any meanning about what i am doing right now 
+but felling great to do that as well
+Again its all about rage 
